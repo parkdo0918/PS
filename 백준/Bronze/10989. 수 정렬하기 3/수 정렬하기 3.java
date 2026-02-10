9f7ef1287ae840c16351acd317081724f9fa7504
Dohyeon Park <parkdo0918@gmail.com>
2026-02-10 15:42:11 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2352 ms, Memory: 363780 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int a[] = new int[n];
+
+        for (int i=0;i<n;i++){
+            a[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(a);
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<n;i++) {
+            sb.append(a[i]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
