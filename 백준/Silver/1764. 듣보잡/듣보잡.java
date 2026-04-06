d926e7d1dd5dc10c1b993ed21680a297abac06a1
Dohyeon Park <parkdo0918@gmail.com>
2026-04-06 13:57:20 +0900

    [Silver IV] Title: 듣보잡, Time: 236 ms, Memory: 26096 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashSet<String> set = new HashSet<>();
+
+        for(int i=0;i<n;i++){
+            set.add(br.readLine());
+        }
+
+
+        List<String> result = new ArrayList<>();
+        for(int i=0;i<m;i++){
+            String name = br.readLine();
+            if(set.contains(name)){
+                result.add(name);
+            }
+        }
+
+        Collections.sort(result);
+
+        sb.append(result.size()).append("\n");
+        for (String name: result){
+            sb.append(name).append("\n");
+        }
+        System.out.println(sb);
+
+    }
+}
